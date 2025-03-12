55e7e03617585c3d11cc81495a338abd0b1f7d85
Slackrr Games | Unblocked <githubstuffs@gmail.com>
2025-03-12 12:36:27 -0500

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,6 @@
 const image = document.querySelector("img"),
   input = document.querySelector("imput");
+
 input.addEventListener("change", () => {
   image.src = URL.createObjectURL(inpur.files[0]);
 });
